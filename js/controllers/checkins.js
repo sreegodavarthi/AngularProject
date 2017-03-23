e3763d0cfd204c1509fe1e2cdbc509ac4a80410b
Sree Godavarthi <sreeg32@gmail.com>
2017-03-23 08:44:31 -0500

    Added form to checkin users

---
+++ js/controllers/checkins.js
@@ -0,0 +1,6 @@
+myApp.controller('CheckInsController', ['$scope', "$rootScope", "$firebaseAuth", "$firebaseArray", 
+      function($scope, $rootScope, $firebaseAuth, $firebaseArray) {
+          
+
+          
+}]);
