33d67f9704f2166ac0fdfd18363d3f2b3b53280d
Sree Godavarthi <sreeg32@gmail.com>
2017-03-22 22:38:22 -0500

    Meetings Records added to database

--- js/controllers/meetings.js
+++ js/controllers/meetings.js
@@ -1,3 +1,24 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-  $scope.message = "Success!!!";
+myApp.controller('MeetingsController', ['$scope', "$firebaseAuth", "$firebaseArray", 
+      function($scope, $firebaseAuth, $firebaseArray) {
+          
+          var ref = firebase.database().ref();
+          var auth = $firebaseAuth();
+
+          auth.$onAuthStateChanged(function(authUser) {
+            if(authUser) {
+              var meetingsRef = ref.child("users").child(authUser.uid).child("meetings");
+              var meetingsInfo = $firebaseArray(meetingsRef);
+
+              $scope.addMeeting = function(){
+                meetingsInfo.$add({
+                  name: $scope.meetingname,
+                  date: firebase.database.ServerValue.TIMESTAMP
+                  }) .then(function(){
+                    $scope.meetingname = ""; 
+                  });//promise
+              } //addmeeting        
+            } 
+
+          });
+          
 }]);
