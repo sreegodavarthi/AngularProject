d5a4b2b13f75fd82b211b40d32977f43dd178c3b
Sree Godavarthi <sreeg32@gmail.com>
2017-06-20 12:15:18 -0400

    Report updated

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,33 +1,17 @@
-myApp.controller('RegistrationController', ['$scope', 'Authentication', '$http',
-    function($scope, Authentication, $http) {
-        $scope.isDataExists = false;
-        $scope.login = function() {
-            Authentication.login($scope.user);
-        };
+myApp.controller('RegistrationController', 
+  ['$scope', 'Authentication', 
+  function($scope, Authentication) {
 
-        $scope.logout = function() {
-            Authentication.logout();
-        };
+  $scope.login = function() {
+    Authentication.login($scope.user);
+  };
 
-        $scope.register = function() {
-            Authentication.register($scope.user);
-        }; //register
+  $scope.logout = function() {
+    Authentication.logout();
+  };
 
-        $scope.getReport = function() {
-            //var url = 'http://demo1.infobuild.in:8080/ibi_apps/run.bip?BIP_REQUEST_TYPE=BIP_RUN&BIP_folder=IBFS%253A%252FWFC%252FRepository%252Fgoogle&BIP_item=CarSalesReport.fex&windowHandle=476704&IBI_random=145.91456346994437';
-            var url = '../views/data.json';
-            $http({
-                method: 'GET',
-                url: url
-            }).
-            success(function(response) {
-                $scope.reportData = response.records;
-                $scope.isDataExists = true;
-            }).
-            error(function(response) {
-                $scope.reportData = response;
-            });
-        };
+  $scope.register = function() {
+    Authentication.register($scope.user);
+  }; //register
 
-    }
-]); //Controller
+}]); //Controller
